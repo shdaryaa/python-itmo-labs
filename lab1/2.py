d36f13cb2f7bfdbe976fbb4e3187e586db576b4e
shdaryaa <shilovadr@gmail.com>
2024-10-16 18:59:02 +0300

    ретактирование цикла

--- lab1/2.py
+++ lab1/2.py
@@ -1,24 +1,28 @@
 WHITE = '\u001b[47m'
 END = '\u001b[0m'
 
-times = 5
+times_x = 5
+times_y = 5
 
-for i in range(5):
-    line = ''
-    for j in range(times):
-        if i != 4 and j != 0:
-            if i == 3:
-                line += f'{' ' * 8}'
+for y in range(times_y):
+    for i in range(5):
+        line = ''
+        for j in range(times_x):
+            if i != 4 and j != 0:
+                if i == 3:
+                    line += f'{' ' * 8}'
+                else:
+                    line += f'{' ' * 4}'
+            if i == 0:
+                line += f'{WHITE}{' ' * 10}{END}'
+            elif i == 1:
+                line += f'{WHITE}{' ' * 2}{END}{' ' * 6}{WHITE}{' ' * 2}{END}'
+            elif i == 2:
+                line += f'{WHITE}{' ' * 2}{END}{' ' * 2}{WHITE}{' ' * 6}{END}'
+            elif i == 3:
+                line += f'{WHITE}{' ' * 2}{END}{' ' * 2}{WHITE}{' ' * 2}{END}'
             else:
-                line += f'{' ' * 4}'
-        if i == 0:
-            line += f'{WHITE}{' ' * 10}{END}'
-        elif i == 1:
-            line += f'{WHITE}{' ' * 2}{END}{' ' * 6}{WHITE}{' ' * 2}{END}'
-        elif i == 2:
-            line += f'{WHITE}{' ' * 2}{END}{' ' * 2}{WHITE}{' ' * 6}{END}'
-        elif i == 3:
-            line += f'{WHITE}{' ' * 2}{END}{' ' * 2}{WHITE}{' ' * 2}{END}'
-        else:
-            line += f'{WHITE}{' ' * 2}{END}{' ' * 2}{WHITE}{' ' * 10}{END}'
-    print(line)
+                line += f'{WHITE}{' ' * 2}{END}{' ' * 2}{WHITE}{' ' * 10}{END}'
+        print(line)
+
+    print()
